8b3f4052ff9482bf378b09d3250bdad9c163b8c2
maq0071 <142011121+maq0071@users.noreply.github.com>
2024-10-17 09:09:01 +0200

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "welcome to the world of java ";
+   private String MESSAGE="Hello world";
 
     public App() {}
 
